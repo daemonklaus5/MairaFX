0833b4187456ad4c12ba581b1c916bd7a2b61a33
sairajss <sairajcrz5@gmail.com>
2026-08-06 15:21:29 +0530

    Fix Express 5 wildcard routing crash

--- backend/src/index.js
+++ backend/src/index.js
@@ -133,7 +133,7 @@ async function bootstrap() {
   app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
   // Catch-all route to serve React index.html for client-side routing
-  app.get('*', (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
   });
 
